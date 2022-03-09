b665b01e4a102566f7503d23e74b16fba623d6b8
arshaddilaver <101076226+arshaddilaver@users.noreply.github.com>
2022-03-09 12:24:37 +0530

    Add files via upload

---
+++ xaaaaaaaaaaaaaaaaaammmmmm.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+    int i,nn,j,k,top,previous,address;
+
+
+void push(int);
+void pop(int);
+
+}
+void push()
+{
+    if (top[stack]=i)
+{
+    printf("Stack is full");
+}
+else
+{
+    printf("enter the element to be inserted:",j);
+    scanf("%d",&j);
+        top=top+1;
+
+        address[nn]=null;
+        address[previous node]*nn;
+
+        printf("push is peformed");
+}
+}
+void pop()
+{
+    if (top=0)
+    {
+        printf("stack is empty");
+    }
+    else
+    {
+      printf("Enter the element to be deleted:",k);
+      scanf("%d",&k);
+      if k=element[stack]
+      top=top-1;
+
+
+
+
+    }
+
+}
+void diplay()
+{
+    diplay()
+
+}
+}
+
+
+
+
+
